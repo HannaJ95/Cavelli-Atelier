c4da5add37dab84dab7da77606d8d1e2171dba5a
Hanna Johansson <hannajohansson16@hotmail.com>
2026-03-10 16:54:41 +0100

    Refactor routes: use resources & auth
    Apply guest middleware to the root (login) view and move logout and dashboard routes inside the auth middleware group. Replace explicit CRUD routes for products, colors and materials with Route::resource(...)->except(['show']) to reduce duplication and simplify route definitions.

--- routes/web.php
+++ routes/web.php
@@ -8,34 +8,15 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ColorController;
 use App\Http\Controllers\MaterialController;
 
-Route::view('/', 'index')->name('login');
-
+Route::view('/', 'index')->middleware('guest')->name('login');
 Route::post('login', LoginController::class);
-Route::post('logout', LogoutController::class)->middleware('auth')->name('logout');
-Route::get('overview', DashboardController::class)->middleware('auth')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-    
-    
-    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
+    Route::post('logout', LogoutController::class)->name('logout');
+    Route::get('overview', DashboardController::class)->name('dashboard');
 
-    Route::get('/colors', [ColorController::class, 'index'])->name('colors.index');
-    Route::get('/colors/create', [ColorController::class, 'create'])->name('colors.create');
-    Route::post('/colors', [ColorController::class, 'store'])->name('colors.store');
-    Route::get('/colors/{color}/edit', [ColorController::class, 'edit'])->name('colors.edit');
-    Route::put('/colors/{color}', [ColorController::class, 'update'])->name('colors.update');
-    Route::delete('/colors/{color}', [ColorController::class, 'destroy'])->name('colors.destroy');
-
-    Route::get('/materials', [MaterialController::class, 'index'])->name('materials.index');
-    Route::get('/materials/create', [MaterialController::class, 'create'])->name('materials.create');
-    Route::post('/materials', [MaterialController::class, 'store'])->name('materials.store');
-    Route::get('/materials/{material}/edit', [MaterialController::class, 'edit'])->name('materials.edit');
-    Route::put('/materials/{material}', [MaterialController::class, 'update'])->name('materials.update');
-    Route::delete('/materials/{material}', [MaterialController::class, 'destroy'])->name('materials.destroy');
+    Route::resource('products', ProductController::class)->except(['show']);
+    Route::resource('colors', ColorController::class)->except(['show']);
+    Route::resource('materials', MaterialController::class)->except(['show']);
 });
