67947e2f4b03f250456cf38eb001813303ad0389
Hanna Johansson <hannajohansson16@hotmail.com>
2026-03-11 15:20:25 +0100

    Move logout form button to sidebar bottom

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -37,7 +37,7 @@
             </div>
         </div>
     </nav>
-    <form method="POST" action="{{ route('logout') }}" class="m-0">
+    <form method="POST" action="{{ route('logout') }}" class="m-0 mt-auto">
         @csrf
         <button type="submit" class="bg-gray-300 hover:bg-gray-400 text-gray-900 px-6 py-2 rounded-lg font-medium transition-colors cursor-pointer">
             Logout
